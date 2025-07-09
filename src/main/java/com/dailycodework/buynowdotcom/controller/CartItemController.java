68ed74f8ff90bce50ee85846f96f818d86ebcd45
show kawazoe <show753@icloud.com>
2025-07-09 17:49:53 +0900

    implementing the cartItem controller

---
+++ src/main/java/com/dailycodework/buynowdotcom/controller/CartItemController.java
@@ -0,0 +1,47 @@
+package com.dailycodework.buynowdotcom.controller;
+
+import com.dailycodework.buynowdotcom.model.Cart;
+import com.dailycodework.buynowdotcom.model.User;
+import com.dailycodework.buynowdotcom.response.ApiResponse;
+import com.dailycodework.buynowdotcom.service.cart.ICartItemService;
+import com.dailycodework.buynowdotcom.service.cart.ICartService;
+import com.dailycodework.buynowdotcom.service.user.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/cartItems")
+public class CartItemController {
+    private final ICartItemService cartItemService;
+    private final IUserService userService;
+    private final ICartService cartService;
+
+    @PostMapping("/item/add")
+    public ResponseEntity<ApiResponse> addItemToCart(
+            Long userId,
+            @RequestParam Long productId,
+            @RequestParam int quantity) {
+        User user = userService.getUserById(userId);
+        Cart cart = cartService.initializeNewCartForUser(user);
+        cartItemService.addItemToCart(cart.getId(), productId, quantity);
+        return ResponseEntity.ok(new ApiResponse("ITem added successfully!", null));
+    }
+
+    @DeleteMapping("/cart/{cartId}/item/{itemId}/remove")
+    public ResponseEntity<ApiResponse> removeItemFromCart(
+            @PathVariable Long cartId, @PathVariable Long itemId
+    ) {
+        cartItemService.removeItemFromCart(cartId, itemId);
+        return ResponseEntity.ok(new ApiResponse("Item removed successfully!", null));
+    }
+
+    @PutMapping("/cart/{cartId}/item/{itemId}/update")
+    public ResponseEntity<ApiResponse> updateCartItem(
+            @PathVariable Long cartId, @PathVariable Long itemId, @RequestParam int quantity
+    ) {
+        cartItemService.updateItemQuantity(cartId, itemId, quantity);
+        return ResponseEntity.ok(new ApiResponse("Item updated successfully!", null));
+    }
+}
