2b2645a5bfdc9d2a38f083b8dd3884412b6d1f9c
show kawazoe <show753@icloud.com>
2025-07-09 18:44:30 +0900

    create the order controller

---
+++ src/main/java/com/dailycodework/buynowdotcom/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.dailycodework.buynowdotcom.controller;
+
+import com.dailycodework.buynowdotcom.model.Order;
+import com.dailycodework.buynowdotcom.response.ApiResponse;
+import com.dailycodework.buynowdotcom.service.order.IOrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/orders")
+public class OrderController {
+    private final IOrderService orderService;
+
+    @PostMapping("/user/order")
+    public ResponseEntity<ApiResponse> placeOrder(@RequestParam Long userId) {
+        Order order = orderService.placeOrder(userId);
+        return ResponseEntity.ok(new ApiResponse("Order placed successfully!", order));
+    }
+
+    @GetMapping("/user/{userId}/order")
+    private ResponseEntity<ApiResponse> getUserOrders(@PathVariable Long userId) {
+        List<Order> orders = orderService.getUserOrders(userId);
+        return ResponseEntity.ok(new ApiResponse("Success!", orders));
+    }
+}
