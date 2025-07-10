79b99b09b44ea7af4e78f33bc4e2acd7f21e474e
show kawazoe <show753@icloud.com>
2025-07-10 17:00:24 +0900

    implementing user security class

--- src/main/java/com/dailycodework/buynowdotcom/repository/UserRepository.java
+++ src/main/java/com/dailycodework/buynowdotcom/repository/UserRepository.java
@@ -4,5 +4,7 @@ import com.dailycodework.buynowdotcom.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  boolean existsByEmail(String email);
+    boolean existsByEmail(String email);
+
+    User findByEmail(String email);
 }
